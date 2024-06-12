3d865cf943852968dea9fb034b46ab409aa205b7
kavidanimesha <kavindanimesha@gmail.com>
2024-06-12 14:44:19 +0530

    Finale commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require ('express')
 const dotenv = require('dotenv');
+const cors = require('cors')
 const connectDB = require('./config/db')
 const { errorHandler } = require('./middleware/errorMiddleware')
 
@@ -13,7 +14,10 @@ const port = process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(express.urlencoded({extended:false}))
+
+
 
 app.use('/api/products', require('./routes/productRoutes'))
 app.use('/api/users', require('./routes/usersRoutes'))
